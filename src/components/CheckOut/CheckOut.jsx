370895aaccb7f542480331fa6785125dc9ee90b5
IanUpton <uptonian22@gmail.com>
2024-09-18 22:08:04 +0100

    cats4lyf upload

---
+++ src/components/CheckOut/CheckOut.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CheckOut.css'; // Add styling in Checkout.css
+
+const CheckOut = () => {
+  return (
+    <div className="checkout">
+      <h1>Awesomeeeeeeeee!!!!!!.....</h1>
+      <h2>Checkout</h2>
+      <p>Thank you for your purchase! This is where the checkout process would take place.</p>
+    <button>checkoutButton</button>
+    </div>
+    
+  );
+};
+
+export default CheckOut;
